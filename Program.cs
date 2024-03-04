271d860ff7be981f630649aef7a67002f77a5a95
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-04 19:33:28 +0100

    reverse function added

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class MyApp
+{
+
+    static class ReversedString
+    {   
+
+
+        static void Main()
+        {
+          //Ask user for input
+          Console.Write("Write a text:");
+          string? text = Console.ReadLine();
+
+          // Check if input in not empty
+          if (!String.IsNullOrEmpty(text))
+          {
+              string reversedText =  reverseString(text: text);
+              Console.WriteLine(reversedText);
+
+          }
+          else
+          {
+              Console.WriteLine("User input is null");
+          }
+        }
+
+        static string reverseString(string text)
+        {
+            // Reverse the string
+            char[] charArry = text.ToCharArray();
+            Array.Reverse(charArry);
+            return new string(charArry);
+        }
+    }
+}
